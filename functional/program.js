1c62e87f500cb7b4a45c3945ca831ff8f215dae9
york zhang <yorkcheung@hotmail.com>
2015-07-02 19:05:34 +0800

    ...

--- functional/program.js
+++ functional/program.js
@@ -1,40 +1,10 @@
-/*function duckCount(){
-	
-	var argvs = Array.prototype.slice.call(arguments);
+function logger(namespace) {
 
-	var a = argvs.filter(function(item, index){
-		return Object.prototype.hasOwnProperty.call(item, 'quack');
-	});
+	function logging(){
+		console.log(namespace, arguments[0]);
+	}
 
-	return a.length;
-
+	return logging.apply()
 }
 
-module.exports = duckCount;*/
-
-function duckCount(){
-	
-	console.log(arguments.length);
-	var argvs = Array.prototype.slice.call(arguments);
-	
-	return (function count(index, num){
-
-		
-		if (index+1 > argvs.length) return num;
-
-		if(Object.prototype.hasOwnProperty.call(argvs[index], 'quack')){
-			
-			num += 1;
-		}
-
-		return count(index+1, num);
-
-
-	})(0, 0);
-
-}
-
-module.exports = duckCount;
-
-
-
+module.exports = logger;
