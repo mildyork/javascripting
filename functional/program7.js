1722c046dd5aac33aaa437d96a58beb44e0c6c80
york zhang <yorkcheung@hotmail.com>
2015-07-02 19:05:07 +0800

    hmm

--- functional/program7.js
+++ functional/program7.js
@@ -1,17 +1,20 @@
-function reduce(arr, fn, initial) {
-	console.log(initial);
-	if (!arr.length) return initial;
-	return reduce(arr.slice(1), fn, fn(initial, arr[0]));
+function reduce(arr, fn, initial){
+
+	return (function reduceOne(index, results){
+		if (index+1 > arr.length) return results;
+
+		return reduceOne(index+1, fn(results, arr[index], index, arr));
+	})(0, initial);
+
 }
 
 module.exports = reduce;
 
 /*function reduce(arr, fn, initial) {
-	return (function reduceOne(index, value) {
-        if (index > arr.length - 1) return value // end condition
-        return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
-    })(0, initial) // IIFE. kick off recursion with initial values
+	console.log(initial);
+	if (!arr.length) return initial;
+	return reduce(arr.slice(1), fn, fn(initial, arr[0]));
 }
 
-module.exports = reduce*/
+
 
